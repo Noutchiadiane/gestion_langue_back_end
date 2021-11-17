baa52c2ec5f06a637108d1e147911da7d09e1c5b
aloedeveloper1 <61685426+Noutchiadiane@users.noreply.github.com>
2021-11-17 14:39:19 +0100

    Generating a seeder script  populate the database

---
+++ seeders/20211117113149-seed-language.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      "Languages",
+      [
+        {
+          title: "fr",
+          speak: 3,
+          understand: 2,
+          read: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
