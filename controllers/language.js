2827a31690708e760f347d3e5602f3ac83e8dd37
aloedeveloper1 <61685426+Noutchiadiane@users.noreply.github.com>
2021-11-17 12:23:10 +0100

    update controller attribut of the fields in the database

--- controllers/language.js
+++ controllers/language.js
@@ -1,142 +1,140 @@
-const {
-    language,
-    Sequelize
-   } = require("./../models");
-   const Op = Sequelize.Op;
-   let self = {};
-   self.getAll = async (req,res) => {
-    try{
-     let data = await language.findAll({
-      attributes:["id","name"]
-     });
-     return res.json({
-      status:"ok",
-      data:data
-     })
-    }catch(error){
-     res.status(500).json({
-      status:"error",
-      data:error
-     })
-    }
-   }
-   self.getWithItems = async (req,res) => {
-    try{
-     let data = await language.findAll({
-      attributes:["id","name"],
-      include:[
-       {
-        model:item,
-        as:'items',
-        attributes:["id","name","price","stock"]
-       }
-      ]
-     });
-     return res.json({
-      status:"ok",
-      data:data
-     })
-    }catch(error){
-     res.status(500).json({
-      status:"error",
-      data:error
-     })
-    }
-   }
-   self.get = async (req,res) => {
-    try{
-     let id = req.params.id;
-     let data = await language.findOne({
-      attributes:["id","name"],
-      where:{
-       id:id
-      }
-     });
-     return res.json({
-      status:"ok",
-      data:data
-     })
-    }catch(error){
-     res.status(500).json({
-      status:"error",
-      data:error
-     })
-    }
-   }
-   self.search = async (req,res) => {
-    try{
-     let text = req.query.text;
-     let data = await language.findAll({
-      attributes:["id","name"],
-      where:{
-       name:{
-        [Op.like]:"%"+text+"%"
-       }
-      }
-     });
-     return res.json({
-      status:"ok",
-      data:data
-     })
-    }catch(error){
-     res.status(500).json({
-      status:"error",
-      data:error
-     })
-    }
-   }
-   self.save = async (req,res) => {
-    try{
-     let body = req.body;
-     let data = await language.create(body);
-     return res.json({
-      status:"ok",
-      data:data
-     })
-    }catch(error){
-     res.status(500).json({
-      status:"error",
-      data:error
-     })
-    }
-   }
-   self.update = async (req,res) => {
-    try{
-     let id = req.params.id;
-     let body = req.body;
-     let data = await language.update(body,{
-      where:{
-       id:id
-      }
-     });
-     return res.json({
-      status:"ok",
-      data:data
-     })
-    }catch(error){
-     res.status(500).json({
-      status:"error",
-      data:error
-     })
-    }
-   }
-   self.delete = async (req,res) => {
-    try{
-     let id = req.params.id;
-     let data = await language.destroy({
-      where:{
-       id:id
-      }
-     });
-     return res.json({
-      status:"ok",
-      data:data
-     })
-    }catch(error){
-     res.status(500).json({
-      status:"error",
-      data:error
-     })
-    }
-   }
-   module.exports = self;
+const { language, Sequelize } = require("./../models");
+const Op = Sequelize.Op;
+let self = {};
+self.getAll = async (req, res) => {
+  try {
+    let data = await language.findAll({
+      attributes: ["title", "speak", "understand", "read"],
+    });
+    return res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      data: error,
+    });
+  }
+};
+
+self.getWithItems = async (req, res) => {
+  try {
+    let data = await language.findAll({
+      attributes: ["title", "speak", "understand", "read"],
+      include: [
+        {
+          model: item,
+          as: "items",
+          attributes: ["title", "speak", "understand", "read"],
+        },
+      ],
+    });
+    return res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      data: error,
+    });
+  }
+};
+self.get = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data = await language.findOne({
+      attributes: ["title", "speak", "understand", "read"],
+      where: {
+        id: id,
+      },
+    });
+    return res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      data: error,
+    });
+  }
+};
+self.search = async (req, res) => {
+  try {
+    let text = req.query.text;
+    let data = await language.findAll({
+      attributes: ["title", "speak", "understand", "read"],
+      where: {
+        name: {
+          [Op.like]: "%" + text + "%",
+        },
+      },
+    });
+    return res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      data: error,
+    });
+  }
+};
+self.save = async (req, res) => {
+  try {
+    let body = req.body;
+    let data = await language.create(body);
+    return res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      data: error,
+    });
+  }
+};
+self.update = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let body = req.body;
+    let data = await language.update(body, {
+      where: {
+        id: id,
+      },
+    });
+    return res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      data: error,
+    });
+  }
+};
+self.delete = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data = await language.destroy({
+      where: {
+        id: id,
+      },
+    });
+    return res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      data: error,
+    });
+  }
+};
+module.exports = self;
