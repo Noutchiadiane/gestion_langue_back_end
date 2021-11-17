1761ebca4fb372a7ed50c2d2ff7b62087a337872
aloedeveloper1 <61685426+Noutchiadiane@users.noreply.github.com>
2021-11-17 12:24:40 +0100

    Create route

---
+++ routes/route.js
@@ -0,0 +1,12 @@
+const language = require("./../controllers/language");
+module.exports = function(express) {
+ const route = express.Router();
+//brands route
+ route.get("/",language.getAll);
+ route.get("/:id",language.get);
+ route.post("/",language.save);
+ route.put("/:id",language.update);
+ route.delete("/:id",language.delete);
+
+ return route;
+};
